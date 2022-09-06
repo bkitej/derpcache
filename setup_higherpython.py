a34cbe94a656e74d8059b8c65af5e7afa2b8fcf8
Cyrus Parvereshi <cyrusParvereshi@users.noreply.github.com>
2022-09-06 02:37:41 -0700

    Create setup_higherpython.py
    This indicates to users using python3.8 the  correct dependencies (no pickle5 since pickle by default installed on those versions, pickle5 dependency will fail build).

---
+++ setup_higherpython.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='derpcache',
+    version='0.0.3',
+    description='A simple pickle-based caching utility.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='GPL-3',
+    author='Ben Johnson',
+    author_email='bkitej@gmail.com',
+    packages=find_packages(),
+    python_requires=">=3.8",
+    extras_require={
+        'dev': [
+            'commitizen',
+            'pre-commit',
+        ],
+        'lint': [
+            'black',
+            'flakeheaven',
+            'mypy',
+            'types-setuptools',
+        ],
+        'test': [
+            'faker',
+            'pytest',
+            'pytest-freezegun',
+        ],
+        'example-test': [
+            'jupyter',
+            'requests'
+        ]
+    },
+    zip_safe=False,  # for mypy to detect typing stubs
+)
